1b90e4a335a078e1068342e5fa1752cf332c1f74
Khanh-Toan-Nguyen <ktoan271199@gmail.com>
2021-12-31 16:51:48 +0700

    add er-mir
    add er-mir

---
+++ models/er-mir.py
@@ -0,0 +1,119 @@
+# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Davide Abati, Simone Calderara.
+# All rights reserved.
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+from utils.buffer import Buffer
+from utils.args import *
+from models.utils.continual_model import ContinualModel
+import copy
+
+
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser(description='Continual learning via'
+                                        ' Experience Replay.')
+    add_management_args(parser)
+    add_experiment_args(parser)
+    add_rehearsal_args(parser)
+    return parser
+
+def get_grad_vector(pp, grad_dims):
+    """
+     gather the gradients in one vector
+    """
+    grads = torch.Tensor(sum(grad_dims)).cuda()
+
+    grads.fill_(0.0)
+    cnt = 0
+    for param in pp():
+        if param.grad is not None:
+            beg = 0 if cnt == 0 else sum(grad_dims[:cnt])
+            en = sum(grad_dims[:cnt + 1])
+            grads[beg: en].copy_(param.grad.data.view(-1))
+        cnt += 1
+    return grads
+
+def overwrite_grad(pp, new_grad, grad_dims):
+    """
+        This is used to overwrite the gradients with a new gradient
+        vector, whenever violations occur.
+        pp: parameters
+        newgrad: corrected gradient
+        grad_dims: list storing number of parameters at each layer
+    """
+    cnt = 0
+    for param in pp():
+        param.grad=torch.zeros_like(param.data)
+        beg = 0 if cnt == 0 else sum(grad_dims[:cnt])
+        en = sum(grad_dims[:cnt + 1])
+        this_grad = new_grad[beg: en].contiguous().view(
+            param.data.size())
+        param.grad.data.copy_(this_grad)
+        cnt += 1
+
+def get_future_step_parameters(this_net, grad_vector, grad_dims, lr=1):
+    """
+    computes \theta-\delta\theta
+    :param this_net:
+    :param grad_vector:
+    :return:
+    """
+    new_net=copy.deepcopy(this_net)
+    overwrite_grad(new_net.parameters,grad_vector,grad_dims)
+    with torch.no_grad():
+        for param in new_net.parameters():
+            if param.grad is not None:
+                param.data=param.data - lr*param.grad.data
+    return new_net
+
+class ErMir(ContinualModel):
+    NAME = 'er-mir'
+    COMPATIBILITY = ['class-il', 'domain-il', 'task-il', 'general-continual']
+
+    def __init__(self, backbone, loss, args, transform):
+        super(ErMir, self).__init__(backbone, loss, args, transform)
+        self.buffer = Buffer(self.args.buffer_size, self.device)
+
+    def observe(self, inputs, labels, not_aug_inputs):
+
+        real_batch_size = inputs.shape[0]
+
+        self.opt.zero_grad()
+        if not self.buffer.is_empty():
+            buf_inputs, buf_labels = self.buffer.get_data(
+                self.args.minibatch_size * 2, transform=self.transform)
+            
+            grad_dims = []
+            for param in self.net.parameters():
+                grad_dims.append(torch.numel(param.data))
+            
+            grad_vector = get_grad_vector(self.net.parameters, grad_dims)
+            model_temp = get_future_step_parameters(self.net, grad_vector, grad_dims, lr = self.args.lr)
+
+            with torch.no_grad():
+                logits_track_pre = self.net(buf_inputs)
+                logits_track_post = model_temp(buf_inputs)
+
+                pre_loss = self.loss(logits_track_pre, buf_labels)
+                post_loss = self.loss(logits_track_post, buf_labels)
+
+                scores = post_loss - pre_loss
+
+                big_ind = scores.sort(descending=True)[1][:self.args.minibatch_size]
+            
+            buf_inputs, buf_labels = buf_inputs[big_ind], buf_labels[big_ind]
+
+            # cat into inputs
+            inputs = torch.cat((inputs, buf_inputs))
+            labels = torch.cat((labels, buf_labels))
+
+        outputs = self.net(inputs)
+        loss = self.loss(outputs, labels)
+        loss.backward()
+        self.opt.step()
+
+        self.buffer.add_data(examples=not_aug_inputs,
+                             labels=labels[:real_batch_size])
+
+        return loss.item()
