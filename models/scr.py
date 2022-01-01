291858d3b54bb7ea76c046c00deb9eefc5695a7e
Khanh-Toan-Nguyen <ktoan271199@gmail.com>
2022-01-01 12:38:45 +0700

    add scr
    add scr

--- models/scr.py
+++ models/scr.py
@@ -1,14 +1,10 @@
-# Copyright 2020-present, Pietro Buzzega, Matteo Boschini, Angelo Porrello, Davide Abati, Simone Calderara.
-# All rights reserved.
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 import torch
 from utils.buffer import Buffer
 from utils.args import *
 from models.utils.continual_model import ContinualModel
 from utils.loss_scr import SupConLoss
 from backbone.ResNet18 import SupConResNet
+import torchvision.transforms as transforms
 
 
 def get_parser() -> ArgumentParser:
@@ -33,8 +29,19 @@ class SCR(ContinualModel):
         self.buffer = Buffer(self.args.buffer_size, self.device)
         self.loss = SupConLoss(temperature=self.args.temper)
         self.net = SupConResNet()
+        self.transform = transforms.Compose([
+                self.transform,
+                transforms.RandomApply(transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1), p = 0.7),
+                transforms.RandomGrayscale(p=0.2)
+            ])
+        self.class_means = None
 
     def observe(self, inputs, labels, not_aug_inputs):
+        if not hasattr(self, 'classes_so_far'):
+            self.register_buffer('classes_so_far', labels.unique().to('cpu'))
+        else:
+            self.register_buffer('classes_so_far', torch.cat((
+                self.classes_so_far, labels.to('cpu'))).unique())
 
         real_batch_size = inputs.shape[0]
 
@@ -57,3 +64,37 @@ class SCR(ContinualModel):
                              labels=labels[:real_batch_size])
 
         return loss.item()
+
+    def compute_class_means(self) -> None:
+        """
+        Computes a vector representing mean features for each class.
+        """
+        # This function caches class means
+        transform = self.dataset.get_normalization_transform()
+        class_means = []
+        examples, labels, _ = self.buffer.get_all_data(transform)
+        for _y in self.classes_so_far:
+            x_buf = torch.stack(
+                [examples[i]
+                 for i in range(0, len(examples))
+                 if labels[i].cpu() == _y]
+            ).to(self.device)
+
+            class_means.append(self.net.features(x_buf).mean(0))
+        self.class_means = torch.stack(class_means)
+
+    def forward(self, x):
+        if self.class_means is None:
+            with torch.no_grad():
+                self.compute_class_means()
+
+        feats = self.net.features(x)
+        feats = feats.unsqueeze(1)
+
+        pred = (self.class_means.unsqueeze(0) - feats).pow(2).sum(2)
+        return -pred
+
+    def end_task(self, dataset) -> None:
+        self.net.train()
+        self.current_task += 1
+        self.class_means = None
