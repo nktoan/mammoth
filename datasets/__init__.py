b48149afa36bb51cd9919b785c8b3f7259145982
Khanh-Toan-Nguyen <ktoan271199@gmail.com>
2022-01-02 13:55:57 +0700

    update datasets
    update datasets

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,6 +6,8 @@
 from datasets.perm_mnist import PermutedMNIST
 from datasets.seq_mnist import SequentialMNIST
 from datasets.seq_cifar10 import SequentialCIFAR10
+from datasets.seq_cifar100 import SequentialCIFAR100
+from datasets.seq_cifar100_20 import SequentialCIFAR100_20
 from datasets.rot_mnist import RotatedMNIST
 from datasets.seq_tinyimagenet import SequentialTinyImagenet
 from datasets.mnist_360 import MNIST360
@@ -16,6 +18,8 @@ NAMES = {
     PermutedMNIST.NAME: PermutedMNIST,
     SequentialMNIST.NAME: SequentialMNIST,
     SequentialCIFAR10.NAME: SequentialCIFAR10,
+    SequentialCIFAR100.NAME : SequentialCIFAR100,
+    SequentialCIFAR100_20.NAME : SequentialCIFAR100_20,
     RotatedMNIST.NAME: RotatedMNIST,
     SequentialTinyImagenet.NAME: SequentialTinyImagenet,
     MNIST360.NAME: MNIST360
