4e70a69a91ba316e6a753c0d44f33d034aa84b8f
Khanh-Toan-Nguyen <ktoan271199@gmail.com>
2022-01-03 17:16:03 +0700

    add buffer tricks for SCR
    add buffer tricks for SCR

--- datasets/seq_cifar100_20.py
+++ datasets/seq_cifar100_20.py
@@ -68,9 +68,9 @@ class SequentialCIFAR100_20(ContinualDataset):
     # color_jitter = transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)
     TRANSFORM = transforms.Compose(
             [
-                transforms.RandomCrop(32, padding=4),
+                # transforms.RandomCrop(32, padding=4),
                 # transforms.RandomApply([color_jitter], p = 0.7),
-                transforms.RandomHorizontalFlip(p = 0.5),
+                # transforms.RandomHorizontalFlip(p = 0.5),
                 # transforms.RandomGrayscale(p = 0.25),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5071, 0.4865, 0.4409),(0.2673, 0.2564, 0.2761))
